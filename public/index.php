5267e645a1c4cfa1ee9e50c714f9c543f50f0264
acmadi <diahmad330@gmail.com>
2023-10-04 21:56:48 +0700

    enhance invalid jwt exceptioin

--- public/index.php
+++ public/index.php
@@ -16,6 +16,14 @@ $loader = require '../vendor/autoload.php';
     use Firebase\JWT\JWT;
     use Firebase\JWT\Key;
 
+    use Firebase\JWT\SignatureInvalidException;
+    use Firebase\JWT\BeforeValidException;
+    use Firebase\JWT\ExpiredException;
+    use DomainException;
+    use InvalidArgumentException;
+    use UnexpectedValueException;
+
+
     use Symfony\Component\Dotenv\Dotenv;
 
     $dotenv = new Dotenv();
@@ -66,20 +74,64 @@ $loader = require '../vendor/autoload.php';
                 echo 'Token not found in request';
                 exit;
             }
-			$jwt = $matches[1]??false;
+			
 
-            $token = JWT::decode($jwt, $secretKey);
-            $now = new DateTimeImmutable();
-            $serverName = "example.org";
-            
-            if (($token?->iss??'') !== $serverName
-                 /* || $token?->nbf??'' > $now->getTimestamp() 
-                 || $token?->exp??0 < $now->getTimestamp() */
-                )
-            {
+            try {
+                $jwt = $matches[1]??false;
+
+                $token = JWT::decode($jwt, $secretKey);
+                $now = new DateTimeImmutable();
+                $serverName = "example.org";
+                
+                if (($token?->iss??'') !== $serverName
+                    /* || $token?->nbf??'' > $now->getTimestamp() 
+                    || $token?->exp??0 < $now->getTimestamp() */
+                    )
+                {
+                    header('HTTP/1.1 401 Unauthorized');
+                    echo '401 Unauthorized';
+                    exit;
+                }
+            } catch (InvalidArgumentException $e) {
                 header('HTTP/1.1 401 Unauthorized');
-                echo '401 Unauthorized';
+                echo '401 Unauthorized InvalidArgumentException';
                 exit;
+                // provided key/key-array is empty or malformed.
+            } catch (DomainException $e) {
+
+                header('HTTP/1.1 401 Unauthorized');
+                echo '401 Unauthorized DomainException';
+                exit;
+                // provided algorithm is unsupported OR
+                // provided key is invalid OR
+                // unknown error thrown in openSSL or libsodium OR
+                // libsodium is required but not available.
+            } catch (SignatureInvalidException $e) {
+
+                header('HTTP/1.1 401 Unauthorized');
+                echo '401 Unauthorized SignatureInvalidException';
+                exit;
+                // provided JWT signature verification failed.
+            } catch (BeforeValidException $e) {
+                header('HTTP/1.1 401 Unauthorized');
+                echo '401 Unauthorized BeforeValidException';
+                exit;
+
+                // provided JWT is trying to be used before "nbf" claim OR
+                // provided JWT is trying to be used before "iat" claim.
+            } catch (ExpiredException $e) {
+                header('HTTP/1.1 401 Unauthorized');
+                echo '401 Unauthorized ExpiredException';
+                exit;
+                // provided JWT is trying to be used after "exp" claim.
+            } catch (UnexpectedValueException $e) {
+                header('HTTP/1.1 401 Unauthorized');
+                echo '401 Unauthorized UnexpectedValueException';
+                exit;
+                // provided JWT is malformed OR
+                // provided JWT is missing an algorithm / using an unsupported algorithm OR
+                // provided JWT algorithm does not match provided key OR
+                // provided key ID in key/key-array is empty or invalid.
             }
             
 		}
