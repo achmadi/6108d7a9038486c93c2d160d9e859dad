43300a0db72746e770af3dc0ce973cbe87f5234d
acmadi <diahmad330@gmail.com>
2023-10-04 18:05:42 +0700

    implements abstract request layer as front controller

--- public/index.php
+++ public/index.php
@@ -1,2 +1,27 @@
 <?php 
-phpinfo();
+/* phpinfo(); */
+
+$loader = require '../vendor/autoload.php';
+    $loader->register();
+
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    
+    $request = Request::createFromGlobals();
+    $response = new Response();
+    
+    switch ($request->getPathInfo()) {
+    case '/':
+    	$response->setContent('This is the website home');
+    	break;
+    
+    	case '/about':
+    		$response->setContent('This is the about page');
+    		break;
+    
+    	default:
+    		$response->setContent('Not found !');
+    	$response->setStatusCode(Response::HTTP_NOT_FOUND);
+    }
+    
+    $response->send();
